b164ea39056a13d6ffff5c8052145568922f2330
ff.davlyaev <DevNet@net.net>
2024-03-24 10:56:05 +0500

    init

---
+++ Dopusteam.Otus.Reflection.ConsoleApp/Examples/ComparerExample.cs
@@ -0,0 +1,33 @@
+﻿using Dopusteam.Otus.Reflection.Comparer;
+
+namespace Dopusteam.Otus.Reflection.ConsoleApp.Examples;
+
+public static class ComparerExample
+{
+    public static void Run()
+    {
+        var first = new ComparableItem("1", "FirstName", "123");
+        var second = new ComparableItem("2", "FirstName", "1234");
+
+        var differences = ObjectsComparer.GetDifference(first, second);
+
+        Console.WriteLine(differences);
+    }
+}
+
+public class ComparableItem
+{
+    public string Id { get; }
+
+    public string Name { get; }
+    
+    public string LastName { get; }
+    
+
+    public ComparableItem(string id, string name, string lastName)
+    {
+        Id = id;
+        Name = name;
+        LastName = lastName;
+    }
+}
---
+++ Dopusteam.Otus.Reflection.ConsoleApp/Examples/TaskExample.cs
@@ -0,0 +1,55 @@
+﻿namespace Dopusteam.Otus.Reflection.ConsoleApp.Examples;
+
+public static class TaskExample
+{
+    public static bool HasDifference(int first, int second)
+    {
+        return first != second;
+    }
+    
+    public static bool HasDifference(Item first, Item second)
+    {
+        return first.Id != second.Id;
+    }
+    
+    public static bool HasDifference(OtherItem first, OtherItem second)
+    {
+        return first.Id != second.Id;
+    }
+    
+    public static bool HasDifference(object first, object second)
+    {
+        var type = first.GetType();
+        var type2 = second.GetType();
+
+        return true;
+    }
+}
+
+#region Приватные сущности
+
+public class Item
+{
+    public int Id { get; }
+
+    public Item(int id)
+    {
+        Id = id;
+    }
+}
+
+#region Вторая
+
+public class OtherItem
+{
+    public int Id { get; }
+
+    public OtherItem(int id)
+    {
+        Id = id;
+    }
+}
+
+#endregion
+
+#endregion
